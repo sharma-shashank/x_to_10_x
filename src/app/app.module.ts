1367485feff9e6bc596dc1e35f13ff5059e7e27e
shashank sharma <shashank.sharma@dealshare.in>
2020-06-13 14:11:04 +0530

    basic project setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from '@ngrx/store';
+import { commonReducer } from './stores/common.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductCardComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    MatListModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatButtonModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ store: commonReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent]
